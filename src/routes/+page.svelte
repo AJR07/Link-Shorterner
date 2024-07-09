9e1e39ae718c765ac3256392c268b8e2cd04f0bc
AJR07 <angjunray@live.com>
2024-07-09 12:15:58 +0800

    :sparkles: Default config

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,20 @@
-<div></div>
+<main>
+	<p class="title">Link Shortener</p>
+	<p class="title small">by AJR07</p>
+</main>
+
+<style>
+    .title {
+        font-weight: bold;
+        font-size: 50px;
+        
+		text-shadow: 0px 0px 20px rgba(132, 205, 125, 0.764);
+
+		width: 100%;
+		text-align: center;
+    }
+
+	.small {
+		font-size: 20px;
+	}
+</style>
